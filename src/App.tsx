b41ad552e172240650ec28319eb45546a66b7a85
HashRei <hashrei@protonmail.com>
2023-02-18 08:27:27 +0100

    Add handling of search change

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      // perform search using searchValue when the timer expires 
+      console.log(`Search for ${searchValue}`);
+    }, 300);
+
+    // Clear the timer if the component is unmounted or if the searchValue changes
+    return () => clearTimeout(timer);
+  }, [searchValue]);
+
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(event.target.value);
+  };
   return (
     <div className="flex items-center justify-center h-screen bg-slate-200">
       <input
         type="text"
         placeholder="Enter text to search GIF"
         name="gifSearch"
+        onChange={handleSearchChange}
       />
     </div>
   );
